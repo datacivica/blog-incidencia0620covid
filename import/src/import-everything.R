c38de5eb444ebab166e026f7d68b16faed845ed2
Adrix <adrian.lara@datacivica.org>
2020-07-04 17:09:16 -0500

    agregamos código para importar archivos

--- import/src/import-everything.R
+++ import/src/import-everything.R
@@ -15,7 +15,7 @@ p_load(tidyverse, janitor, here, data.table, readxl)
 
 files <- list(input_idenm = here("import/input/IDEFC_NM_may2020.csv"),
               input_idmnm = here("import/input/IDM_NM_may2020.csv"),
-              input_nombres = here("import/input/nombres_municipios.xlsx.gz"),
+              input_nombres = here("import/input/nombres_municipios.xlsx"),
               input_pob_est = here("import/input/pob_mit_proyecciones.csv.gz"),
               input_pob_mun = here("import/input/municipios_sexo_15-30.csv.gz"),
               output_idenm = here("import/output/idenm.rds"),
@@ -42,7 +42,7 @@ df_idmnm  <- read.csv(files$input_idmnm, fileEncoding = "ISO-8859-1",
   mutate(clave_ent = formatC(clave_ent, width = 2, flag = "0", format = "d"),
          cve_municipio = formatC(cve_municipio, width = 5, flag = "0", format = "d"))
 
-df_nombres1 <- read_excel(gunzip(files$input_nombres, overwrite=T, remove=F)) %>% 
+df_nombres1 <- read_excel(files$input_nombres) %>% 
   clean_names() %>% 
   distinct(cve_ent, .keep_all = T) %>% 
   select(cve_ent, nom_ent) %>% 
@@ -53,7 +53,7 @@ df_nombres1 <- read_excel(gunzip(files$input_nombres, overwrite=T, remove=F)) %>
            str_detect(nom_ent, "Veracruz") == T ~ "Veracruz",
            T ~ nom_ent)) 
 
-df_nombres2 <- read_excel(gunzip(files$input_nombres, overwrite=T, remove=F)) %>% 
+df_nombres2 <- read_excel(files$input_nombres) %>% 
   clean_names() %>% 
   mutate(inegi = paste0(cve_ent, cve_mun)) %>% 
   mutate(nom_ent = case_when(
@@ -67,11 +67,16 @@ df_nombres2 <- read_excel(gunzip(files$input_nombres, overwrite=T, remove=F)) %>
 df_pobest <- fread(files$input_pob_est, stringsAsFactors = F) %>% 
   mutate(cve_geo = formatC(cve_geo, width = 2, flag = "0", format = "d"))
 
+df_pobmun <- fread(files$input_pob_mun, stringsAsFactors = F) %>% 
+  mutate(clave_ent = formatC(clave_ent, width = 2, flag = "0", format = "d"),
+         clave = formatC(clave, width = 5, flag = "0", format = "d"), )
 
-saveRDS(df_cenapi, files$output_cenapi)
+
+
 saveRDS(df_idenm, files$output_idenm)
 saveRDS(df_idmnm, files$output_idmnm)
 saveRDS(df_nombres1, files$output_nombres1)
 saveRDS(df_nombres2, files$output_nombres2)
 saveRDS(df_pobest, files$output_pob_est)
+saveRDS(df_pobest, files$output_pob_mun)
 
